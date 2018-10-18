6cc890288688d15f1a9ed999332c0c7cc343d106
Martho Evan <evanom59@gmail.com>
2018-10-18 09:51:53 +0200

    Test Heroku

--- src/main/java/com/example/demo/ressource/AnswerRessource.java
+++ src/main/java/com/example/demo/ressource/AnswerRessource.java
@@ -2,8 +2,14 @@ package com.example.demo.ressource;
 
 import com.example.demo.entity.Answer;
 import com.example.demo.service.AnswerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 
 @RestController()
 @RequestMapping("/api")
@@ -14,6 +20,13 @@ public class AnswerRessource {
     public AnswerRessource(AnswerService answerService) {
         this.answerService = answerService;
     }
+
+    /*@RequestMapping(method = GET, path = "/answers/{id}")
+    public ResponseEntity<Answer> findById(@PathVariable Long id) {
+        Optional<Answer> answer = answerService.findById(id);
+        return answer.map(response -> ResponseEntity.ok().body(response))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }*/
 
     @GetMapping("/answers")
     public List<Answer> findAll() {
